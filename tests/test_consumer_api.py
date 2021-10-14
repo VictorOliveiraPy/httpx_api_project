cfd9de1423ea28ae9eedff86f5079797e44f20ec
Victor Hugo Nascimento Oliveira <victor.oliveira@nb-will0492.avista.local>
2021-10-14 17:00:15 -0300

    test: testing mock http request

--- tests/test_consumer_api.py
+++ tests/test_consumer_api.py
@@ -1,7 +1,9 @@
 from src.client.consumer_api import SwapiApiCOnsumer
 
 
-def test_get_starships():
+def test_get_starships(requests_mock):
+    requests_mock.get('https://swapi.dev/api/starships/', status_code=200, json={'some': 'thing'})
+
     api_consumer = SwapiApiCOnsumer()
     resp = api_consumer.get_starships(page=1)
 
